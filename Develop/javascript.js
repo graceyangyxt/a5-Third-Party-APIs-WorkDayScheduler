0201465053748ae32ab3c11fc738707035695a53
Grace Yang <graceyxt.artdesign@gmail.com>
2021-03-06 23:51:11 -0500

    draft1

---
+++ Develop/javascript.js
@@ -0,0 +1,53 @@
+// button
+ //get a reference to all of the save buttons $('.saveBtn')
+ //01 02 jQuery element
+ 
+
+ // add a click handler to each saveButton using .on() method
+ //03 04 click event- http://api.jquery.com/on/
+
+ // traverse the DOM to get the values that we need to store in localstorage .parent() .sibling()
+ //07 08 dom traversal- https://api.jquery.com/category/traversing/
+ 
+ //setItems to localstorage
+ //week 04
+
+
+//handle color coding
+  // use moment library to get the current
+  // http://momentjs.com/docs/#/get-set/hour
+
+  // get a reference to all timeblocks
+
+  // loop through the timeblocks
+  //week3
+
+     //get the data-hour value timeblocks[i].data('hour')
+
+     // if,else -condition we compare the currentHour with the timeblock <(past) ===(present),else(future)
+     //week3
+
+     // adding or removing (or both) classes
+    
+
+//get the data from localstorage and populate the timeblocks
+ //geItem 
+
+ //reference to the textarea (id)
+
+ // .val()
+
+   //approach 
+   //--> $('textarea'); 
+   //--> loop 
+   // get a reference to the id of .parent()
+   //--> .val()=localStorage.getItem(parentID)
+
+// current day--> top of calendar
+  // moment library - .format()
+  // 25 26 moment-format- http://momentjs.com/docs/#/displaying
+
+// call the colorcoding function
+// setInterval ->15000
+// week4
+
